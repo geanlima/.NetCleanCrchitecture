2894180cb65501df735bca7527e804f51c6e65ab
Gean da Silva lima <geansilvalima@hotmail.com>
2022-01-22 14:32:59 -0300

    Adicao na API Product Controller

---
+++ CleanArchMvc.API/Controllers/ProductsController.cs
@@ -0,0 +1,96 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly IProductService _productService;
+        public ProductsController(IProductService productService)
+        {
+            _productService = productService;            
+
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductDTO>>> Get()
+        {
+            var product = await _productService.GetProducts();
+
+            if (product == null)
+            {
+                return NotFound("Products not found");
+            }
+            else
+            {
+                return Ok(product);
+            }
+
+        }
+
+        [HttpGet("{id:int}", Name = "GetProduct")]
+        public async Task<ActionResult<ProductDTO>> Get(int id)
+        {
+            var product = await _productService.GetById(id);
+
+            if (product == null)
+            {
+                return NotFound("Product not found");
+            }
+            else
+            {
+                return Ok(product);
+            }
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] ProductDTO productDto)
+        {
+            if (productDto == null)
+                return BadRequest("Invalid Data");
+
+            await _productService.Add(productDto);
+
+            return new CreatedAtRouteResult("GetProduct", new { id = productDto.Id }, productDto);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Put(int id, [FromBody] ProductDTO productDto)
+        {
+            if (id != productDto.Id)
+                return BadRequest();
+
+            if (productDto == null)
+                return BadRequest();
+
+            await _productService.Update(productDto);
+
+            return Ok(productDto);
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<ActionResult<ProductDTO>> Put(int id)
+        {
+            var product = await _productService.GetById(id);
+
+            if (product == null)
+            {
+                return NotFound("Product not found");
+            }
+            else
+            {
+                await _productService.Remove(id);
+
+                return Ok(product);
+            }
+        }
+    }
+}
--- CleanArchMvc.WebUi/Controllers/ProductsController.cs
+++ CleanArchMvc.WebUi/Controllers/ProductsController.cs
@@ -6,9 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace CleanArchMvc.WebUi.Controllers
